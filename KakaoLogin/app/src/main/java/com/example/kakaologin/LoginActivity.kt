8c55925bd5c84b774b530e77af3bc73f14c6bbe5
mmihye <smilehye910@naver.com>
2023-07-02 02:21:51 +0900

    카카오 로그인 코드 추가

--- KakaoLogin/app/src/main/java/com/example/kakaologin/LoginActivity.kt
+++ KakaoLogin/app/src/main/java/com/example/kakaologin/LoginActivity.kt
@@ -2,13 +2,25 @@ package com.example.kakaologin
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kakaologin.databinding.ActivityLoginBinding
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
 
 class LoginActivity :AppCompatActivity() {
-
     private lateinit var binding: ActivityLoginBinding
+
+    private val callback:(OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if(error != null){
+            //로그인 실패
+        }else if(token != null){
+            //로그인 성공
+            Log.e("loginActivity", "lgoin in with kakao account token == $token")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +28,28 @@ class LoginActivity :AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.kakaoTalkLoginButton.setOnClickListener {
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)){
+                //카카오톡 로그인
+                UserApiClient.instance.loginWithKakaoTalk(this){ token, error ->
+                    if(error != null) {
+                        // 카카오톡 로그인 실패
+                        // 유저가 로그인 닫기
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                    }else if(token != null){
+                        // 로그인 성공
+                        Log.e("loginActivity", "token == $token")
+                    }
+                }
+            }else{
+                //카카오계정 로그인
+
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+        }
 
 
     }
