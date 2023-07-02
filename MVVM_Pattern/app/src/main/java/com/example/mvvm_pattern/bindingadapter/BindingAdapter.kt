6341270b4c2f81e0f9d8bc78d15bf8da2a78afdf
mmihye <smilehye910@naver.com>
2023-07-02 20:33:09 +0900

    초기 세팅

---
+++ MVVM_Pattern/app/src/main/java/com/example/mvvm_pattern/bindingadapter/BindingAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.mvvm_pattern.bindingadapter
+
+import android.graphics.Color
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import coil.load
+import com.example.mvvm_pattern.model.Image
+
+@BindingAdapter("image")
+fun ImageView.setImage(image: Image){
+    if(image == null){
+        return
+    }
+
+    setBackgroundColor(Color.parseColor(image.color))
+
+    load(image.url){
+        crossfade(300)
+    }
+}
