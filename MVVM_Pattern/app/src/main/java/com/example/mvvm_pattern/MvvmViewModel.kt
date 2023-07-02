c356420224bd1d5e5f0841149927069c536120e5
mmihye <smilehye910@naver.com>
2023-07-02 20:45:23 +0900

    add viewModel

---
+++ MVVM_Pattern/app/src/main/java/com/example/mvvm_pattern/MvvmViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.mvvm_pattern
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.mvvm_pattern.model.Image
+import com.example.mvvm_pattern.repository.ImageRepository
+import io.reactivex.disposables.CompositeDisposable
+
+class MvvmViewModel(private val imageRepository: ImageRepository):ViewModel() {
+
+    private val _countLiveData = MutableLiveData<String>()
+    val countLiveData : LiveData<String> by lazy { _countLiveData }
+
+    private val _imageLiveData = MutableLiveData<Image>()
+    val imageLiveData : LiveData<Image> by lazy { _imageLiveData }
+
+    private var disposable: CompositeDisposable? = CompositeDisposable()
+    private var count = 0
+
+    fun loadRandomImage() {
+        disposable?.add(imageRepository.getRandomImage()
+            .doOnSuccess {
+                count++
+            }
+            .subscribe{ item ->
+                _imageLiveData.value = item
+                _countLiveData.value = "불러온 이미지 수 : $count"
+            })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable = null
+    }
+
+    class MvvmViewModelFactroy(private val imageRepository: ImageRepository) :
+            ViewModelProvider.Factory{
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return MvvmViewModel(imageRepository) as T
+        } }
+}
