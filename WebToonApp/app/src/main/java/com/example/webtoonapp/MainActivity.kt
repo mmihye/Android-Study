87869be47031fb36220135af5a23ec479908d25e
mmihye <smilehye910@naver.com>
2023-08-19 23:28:35 +0900

    tabLayout과 viewPager로 변경

--- WebToonApp/app/src/main/java/com/example/webtoonapp/MainActivity.kt
+++ WebToonApp/app/src/main/java/com/example/webtoonapp/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.webtoonapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.TextView
 import com.example.webtoonapp.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,27 +17,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button1.setOnClickListener {
-            // transaction : 작업의 단위
-            // beginTransaction : 작업을 시작, commitTransaction: 작업을 끝
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainer, WebViewFragment())
-                commit()
-            }
-        }
+        binding.viewPager.adapter = ViewPagerAdapter(this)
 
-        binding.button2.setOnClickListener {
-            // transaction : 작업의 단위
-            // beginTransaction : 작업을 시작, commitTransaction: 작업을 끝
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainer, BFragment())
-                commit()
+        TabLayoutMediator(binding.tabLayout, binding.viewPager){ tab, position->
+            run{
+                val textView = TextView(this@MainActivity)
+                textView.text ="position $position"
+                textView.gravity = Gravity.CENTER
+
+                tab.customView = textView
             }
-        }
+        }.attach()
+
+//        binding.button1.setOnClickListener {
+//            // transaction : 작업의 단위
+//            // beginTransaction : 작업을 시작, commitTransaction: 작업을 끝
+//            supportFragmentManager.beginTransaction().apply {
+//                replace(R.id.fragmentContainer, WebViewFragment())
+//                commit()
+//            }
+//        }
+//
+//        binding.button2.setOnClickListener {
+//            // transaction : 작업의 단위
+//            // beginTransaction : 작업을 시작, commitTransaction: 작업을 끝
+//            supportFragmentManager.beginTransaction().apply {
+//                replace(R.id.fragmentContainer, BFragment())
+//                commit()
+//            }
+//        }
     }
 
         override fun onBackPressed() {
-            val currentFragment = supportFragmentManager.fragments.first()
+            val currentFragment = supportFragmentManager.fragments[binding.viewPager.currentItem]
             // webView 안에서 뒤로가기시 여러번클릭으로 webView 내 뒤로갈곳이 있다면
             // webView뒤로가기 없다면 activity backPressed로 앱이 종료
             if(currentFragment is WebViewFragment){
---
+++ WebToonApp/app/src/main/java/com/example/webtoonapp/ViewPagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.webtoonapp
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(private val mainActivity: MainActivity):FragmentStateAdapter(mainActivity) {
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0->{
+                return WebViewFragment()
+            }
+            1->{
+                return BFragment()
+            }
+            else->{
+                return WebViewFragment()
+            }
+        }
+    }
+
+}
