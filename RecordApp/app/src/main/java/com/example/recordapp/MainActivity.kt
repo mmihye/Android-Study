419b7e6a6ea21651d6f8f43c852a9734dd719aed
mmihye <smilehye910@naver.com>
2023-08-20 23:27:39 +0900

    [#1] viewBinding 설정

--- RecordApp/app/src/main/java/com/example/recordapp/MainActivity.kt
+++ RecordApp/app/src/main/java/com/example/recordapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.recordapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recordapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
